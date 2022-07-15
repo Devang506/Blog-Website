6639e0c50545afc7a2f19589ea91d0b25fd05df2
devangjoshi <devangjoshi1210@gmail.com>
2022-07-15 20:26:48 +0530

    Authentication added

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const date = require(__dirname+"/date.js");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const {body, checkSchema, validationResult} = require("express-validator");
+const findOrCreate = require("mongoose-find-or-create");
+let alert = require('alert');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -17,35 +23,134 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(session({
+  secret : "Our little secret.",
+  resave : false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 // coonnecting to database
 mongoose.connect("mongodb+srv://Devangjoshi1210:Devang12345@atlascluster.kmp6b.mongodb.net/blogDBv2");
 
- // creating Schema
- const postSchema = new mongoose.Schema({
-   date :  String,
-   author : String,
-   title:String,
-   content:String
- });
- //creating model
- const Post = mongoose.model("Post", postSchema);
+
+const registrationSchema = {
+  password: {
+       isStrongPassword: {
+           minLength: 8,
+           minLowercase: 1,
+           minUppercase: 1,
+           minNumbers: 1
+       },
+       errorMessage: "Password must be greater than 8 and contain at least one uppercase letter, one lowercase letter, and one number",
+   }
+}
+const userSchema = new mongoose.Schema({
+
+  email: {
+    type: String,
+    unique: true,
+    index:true,
+    sparse:true
+  },
+  username: {
+    type: String,
+
+     unique: true,
+     index:true,
+     sparse:true
+  },
+  password: {
+       type: String
+     },
+     googleId:String
+
+});
+const postSchema = new mongoose.Schema({
+  date :  String,
+  author : String,
+  title:String,
+  content:String,
+  id : String
+});
+//creating model
 
 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-app.get("/",function(req,res){
+const User = new mongoose.model("User",userSchema);
+const Post = mongoose.model("Post", postSchema);
 
-  Post.find({}, function(err, posts){
+const LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(User.authenticate()));
 
-     res.render("home", {
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(function(user, done) {
+  done(null,user.id);
+});
 
-       startingContent: homeStartingContent,
-
-       posts: posts
-
-       });
-
-   })
+passport.deserializeUser(function(id, done) {
+User.findById(id,function(err,user){
+  done(err,user);
 })
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/home",
+    userProfileUrl: "https://googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+
+
+
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }));
+
+  app.get("/auth/google/home",
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect("/home");
+    });
+
+    app.get("/",function(req,res){
+       res.render("landingPage");
+
+    })
+    app.get("/home",function(req,res){
+
+      if(req.isAuthenticated()){
+      
+        Post.find({}, function(err, posts){
+
+           res.render("home", {
+
+             startingContent: "",
+
+             posts: posts,
+             userId:req.user.id
+
+             });
+
+         })
+      }else{
+        res.render("landingPage");
+      }
+
+    })
 app.get("/about",function(req,res){
   res.render("about", {startingAbout : aboutContent});
 
@@ -60,24 +165,102 @@ app.post("/home",function(req,res){
 app.get("/compose",function(req,res){
   res.render("compose");
 })
+app.get("/register",function(req,res){
+  res.render("register");
+})
+app.get("/login",function(req,res){
+  res.render("login");
+})
+app.get("/logout",function(req,res,next){
+  req.logout(function(err) {
+     if (err) { return next(err); }
+       res.render("landingPage");
+   });
+
+})
+app.post("/register", checkSchema(registrationSchema),function(req,res){
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+       alert("Password must be greater than 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.");
+       res.redirect("/register");
+   }else{
+     const user = new User({
+       username : req.body.username,
+       email:req.body.email
+     });
+     User.register(user,req.body.password,function(err,user){
+       if(err){
+
+         alert("A user with given username or Email already exist");
+         res.redirect("/register");
+       }else{
+
+         passport.authenticate("local")(req,res,function(){
+                   res.redirect("/home");
+         })
+
+       }
+     })
+   }                  // comes from passportLocalMongoose
+});
+
+app.post("/login", function(req,res){
+
+  if(!req.body.username){
+           alert( "Username was not given");
+          res.redirect("/login");
+  }else{
+    if(!req.body.password){
+           alert( "Password was not given");
+           res.redirect("/login");
+        }
+        else{
+        passport.authenticate("local", (err, user, info) => {
+        if (err) throw err;
+        if (!user) {
+
+         alert("The username and/or password you specified are not correct.")
+         res.redirect("/login");
+        }
+       else {
+       req.logIn(user, (err) => {
+       if (err) console.log(err);
+       res.redirect("/home");
+  });
+}
+})(req, res);
+
+    }
+      }
+    });
+
+
+
+
+
 app.post("/compose",function(req,res){
+   if(req.isAuthenticated()){
    var d= date.getDate();
    const post = new Post({
       date : d,
       author : req.body.displayName,
       title: req.body.postTitle,
-      content  : req.body.postBody
+      content  : req.body.postBody,
+      id : req.user.id
    });
 
    post.save(function(err){
 
    if (!err){
 
-     res.redirect("/");
+     res.redirect("/home");
 
    }
 
  });
+}else{
+  res.render("landingPage");
+}
 
 
 
@@ -105,7 +288,7 @@ app.post("/delete",function(req,res){
  Post.findOneAndRemove({_id:postId},function(err){
    if(!err)
    {
-     res.redirect("/");
+     res.redirect("home");
    }
  })
 
